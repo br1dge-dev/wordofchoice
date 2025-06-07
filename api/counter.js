de46723c3afab2cdf0ade5b22120777e9bda80c1
br1dge-dev <br1dge.burn3r@gmail.com>
2025-06-08 00:21:09 +0200

    Nutze natives fetch statt node-fetch für Vercel-Kompatibilität (Counter-API)

--- api/counter.js
+++ api/counter.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
 
 const SUPABASE_URL = 'https://kuatsdzlonpjpddcgvnm.supabase.co';
 const SUPABASE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1YXRzZHpsb25wanBkZGNndm5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzMzIyMDIsImV4cCI6MjA2NDkwODIwMn0.A-LrULN8IXTA1HMz0lD-f8-Dpu7fUnJckRsi26uU094';
